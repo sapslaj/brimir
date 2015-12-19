b92a5b1191e94e920403e9414c05eaef2931c0eb
sapslaj <saps.laj@gmail.com>
2015-12-19 14:31:40 -0500

    Added fuzzy searching to Client

--- app/models/client.rb
+++ app/models/client.rb
@@ -1,7 +1,15 @@
 class Client < ActiveRecord::Base
   has_many :tickets, dependent: :destroy
+  fuzzily_searchable :name, :address, :phone_number
 
   scope :ordered, -> {
     order(:name)
   }
+
+  def self.search(query, *fields)
+    fields = [:name] if fields.empty?
+    fields.map do |field|
+      send("find_by_fuzzy_#{field}", query)
+    end.flatten
+  end
 end
