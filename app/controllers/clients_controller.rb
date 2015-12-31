60745a5a444c03d65211693b9c737f6810239542
sapslaj <saps.laj@gmail.com>
2015-12-30 23:09:33 -0500

    Allow email address through ClientsController

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -81,6 +81,6 @@ class ClientsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def client_params
-      params.require(:client).permit(:name, :address, :phone_number, :notes)
+      params.require(:client).permit(:name, :address, :phone_number, :notes, :email_address)
     end
 end
